c460ec4ebf11cad82a14acb2817c0f91b15f84b3
Ashish-wel <ashishnagda34@gmail.com>
2025-08-21 21:54:36 +0530

    Create task 2.py

---
+++ task 2.py
@@ -0,0 +1,12 @@
+First_name = input("Enter your First name: ")
+Last_name = input("Enter your Last name: ")
+
+Full_name = First_name + " " + Last_name
+
+print("\nHello," Full_name + "! Welcome to Python programming ")
+
+-- Output --
+Enter your first name: Alex
+Enter your last name: Denny
+
+Hello, Alex Denny! Welcome to Python programming.
