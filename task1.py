853c3dbee762509364f57140dcaf148f79e70777
Ashish-wel <ashishnagda34@gmail.com>
2025-08-21 21:45:15 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,19 @@
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a * b)
+
+
+--Output--
+Enter the first number: 5
+Enter the second number: 10
+
+Addition: 15
+Subtraction: -5
+Multiplication: 50
+Division: 0.5
